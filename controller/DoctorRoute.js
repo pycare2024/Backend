3876026a9bdafed2e1faa7a3665004cd4b9f0bdf
Dhruv Agarwal <dhruvagarwal@MacBookAir.lan>
2025-03-05 11:13:03 +0530

    updates

--- controller/DoctorRoute.js
+++ controller/DoctorRoute.js
@@ -7,7 +7,7 @@ const DoctorRoute = express.Router();
 // Set up storage for uploaded files
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, "uploads/"); // Store files in 'uploads' folder
+        cb(null, "Uploads/"); // Store files in 'uploads' folder
     },
     filename: (req, file, cb) => {
         cb(null, `${Date.now()}-${file.originalname}`); // Unique filename

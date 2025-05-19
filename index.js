d8e2b100b46a2a8ac261dc6c569bd6c9786aeec8
Dhruv Agarwal <dhruvagarwal@MacBookAir.lan>
2025-05-19 11:42:22 +0530

    updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const express=require("express");
 const mongoose=require("mongoose");
-const patientRoute=require("./controller/patientRoute");
+const patientRoute = require("./controller/patientRoute");
 const AdminRoute=require("./controller/AdminRoute");
 const DoctorRoute=require("./controller/DoctorRoute");
 const QueryRoute=require("./controller/QueryRoute");
